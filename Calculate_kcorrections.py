cbd77362a6e6a90755176639c7c01a2d906fd095
lrhunt <lrhunt@users.noreply.github.com>
2018-10-22 20:35:22 -0400

    Now includes jupyter notebook

--- Calculate_kcorrections.py
+++ Calculate_kcorrections.py
@@ -1,10 +1,10 @@
-#*********************************************************
+
 # Trying to get k-corrections for LCBG in cluster data. 
 # Testing with Molino et al. catalog 2017 for cluster 
 # MACS2129.
-#*********************************************************
-# Editor	Date			Change
-#*********************************************************
+#
+#Editor	Date			Change
+#
 # LH		10/12/2018		First write
 
 import numpy as np
@@ -16,15 +16,11 @@ infile='/home/lrhunt/Documents/LCBG_CLUSTER_GROUP/hlsp_clash_hst_ir_macs2129_cat
 
 dataset=pd.read_table(infile,delim_whitespace=True,header=133)
 
-#****************************************
 # Example of how to select rows in pandas
-#****************************************
 
 cluster_member=dataset[(dataset.zb_1<dataset.clusterz+0.2)&(dataset.zb_1>dataset.clusterz-0.2)]
 
-#****************************************
 # Example of how to select columns in pandas
-#****************************************
 
 cluster_member_photometry=cluster_member[['CLASHID','F435W_ACS_MASS','dF435W_ACS_MASS','F475W_ACS_MASS','dF475W_ACS_MASS','F606W_ACS_MASS','dF606W_ACS_MASS','F625W_ACS_MASS','dF625W_ACS_MASS','F775W_ACS_MASS','dF775W_ACS_MASS','F814W_ACS_MASS','dF814W_ACS_MASS','F850LP_ACS_MASS','dF850LP_ACS_MASS','F105W_WFC3_MASS','dF105W_WFC3_MASS','F110W_WFC3_MASS','dF110W_WFC3_MASS','F125W_WFC3_MASS','dF125W_WFC3_MASS','F140W_WFC3_MASS','dF140W_WFC3_MASS','F160W_WFC3_MASS','dF160W_WFC3_MASS','F225W_WFC3_MASS','dF225W_WFC3_MASS','F275W_WFC3_MASS','dF275W_WFC3_MASS','F336W_WFC3_MASS','dF336W_WFC3_MASS','F390W_WFC3_MASS','dF390W_WFC3_MASS','zb_1']]
 
@@ -50,14 +46,10 @@ cluster_member_maggies_from_kcorr.insert(loc=0,column='redshift',value=np.nan)
 
 cmm_ind=cluster_member_maggies.index.values
 
-#****************************************************************
-#****************************************************************
 # May be able to create a new filter list based on the column names
 # that are already loadable from the text file. If that is possible,
 # don't have to make a bunch of different templates. May need to
 # write a new file.
-#****************************************************************
-#****************************************************************
 
 kcorrect.load_templates()
 kcorrect.load_filters('/home/lrhunt/programs/kcorrect/data/templates/LCBG_CLUSTER_FLITS.dat')
